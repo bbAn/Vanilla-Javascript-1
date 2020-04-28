e7030ea67e217c4932e95728c104282de9f9421d
남진경 <damaskrose83@gmail.com>
2020-04-28 12:47:45 +0900

    Update bg.js

--- js/bg.js
+++ js/bg.js
@@ -8,18 +8,18 @@ function handleImgLoad(){
 }
 
 function paintImage(imgNumber){
-    const image = new Image();
-    image.src = `images/${imgNumber + 1}.jpg`;
-    image.classList.add('bgImage');
-		body.appendChild(image);
-		image.addEventListener('load', handleImgLoad);
+	const image = new Image();
+	image.src = `images/${imgNumber + 1}.jpg`;
+	image.classList.add('bgImage');
+	body.appendChild(image);
+	image.addEventListener('load', handleImgLoad);
 }
 
 function getRandom(){
-		const number = Math.floor(Math.random() * IMG_NUMBER);
-		// Math.floor() 버림함수
-		// Math.ceiling() 올림함수
-    return number;
+	const number = Math.floor(Math.random() * IMG_NUMBER);
+	// Math.floor() 버림함수
+	// Math.ceiling() 올림함수
+	return number;
 }
 
 function init(){
